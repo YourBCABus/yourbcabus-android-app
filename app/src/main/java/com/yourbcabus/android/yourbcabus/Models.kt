8faafc63e16813ed8eb787308b4caac308e9daaf
Anthony Li <li.anthony917@gmail.com>
2019-03-11 17:32:41 -0400

    Write custom ISO date parser

--- app/src/main/java/com/yourbcabus/android/yourbcabus/Models.kt
+++ app/src/main/java/com/yourbcabus/android/yourbcabus/Models.kt
@@ -3,21 +3,64 @@ package com.yourbcabus.android.yourbcabus
 import com.beust.klaxon.Converter
 import com.beust.klaxon.JsonValue
 import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 @Target(AnnotationTarget.FIELD)
 annotation class KlaxonDate {
+    class InvalidDateException: Exception()
+
     companion object : Converter {
         override fun canConvert(cls: Class<*>): Boolean = cls == Date::class.java
 
+        private fun parseTimezone(str: String): TimeZone = when (str) {
+            "Z" -> TimeZone.getTimeZone("UTC")
+            else -> TimeZone.getTimeZone(str)
+        }
+
         override fun fromJson(jv: JsonValue): Any? {
-            throw Exception()
+            try {
+                val str = jv.string!!.replace("-", "").replace(":", "").replace(".", "")
+
+                val year = str.substring(0..3).toInt()
+                val month = str.substring(4..5).toInt()
+                val day = str.substring(6..7).toInt()
+
+                var hour = 0
+                var minute = 0
+                var second = 0
+                var millisecond = 0
+                var tz: TimeZone = TimeZone.getTimeZone("UTC")
+
+                if (str.length > 8) {
+                    hour = str.substring(9..10).toInt()
+                    minute = str.substring(11..12).toInt()
+                    second = str.substring(13..14).toInt()
+
+                    if (str.length > 15) {
+                        if (str[15].isDigit()) {
+                            millisecond = str.substring(15..17).toInt()
+                            if (str.length > 18) {
+                                tz = parseTimezone(str.substring(18))
+                            }
+                        } else {
+                            tz = parseTimezone(str.substring(15))
+                        }
+                    }
+                }
+
+                val calendar: Calendar = GregorianCalendar()
+                calendar.timeZone = tz
+                calendar.set(year, month - 1, day, hour, minute, second)
+                calendar.set(Calendar.MILLISECOND, millisecond)
+
+                return calendar.time
+            } catch (e: Exception) {
+                throw InvalidDateException()
+            }
         }
 
         override fun toJson(value: Any): String {
-            throw Exception()
+            TODO()
         }
     }
 }
